33015f2be839d3f8e0958544525c14e5baefe6c4
anmolmahajan02 <anmolmahajan236@gmail.com>
2025-06-17 23:06:30 +0530

    Nothing much just some loops

--- blog/models.py
+++ blog/models.py
@@ -6,5 +6,6 @@ from django.contrib.auth.models import User
 class post(models.Model):
     user = models.ForeignKey(User,on_delete=models.CASCADE)
     title = models.CharField(max_length=100)
-    discription = models.TextField(max_length=1000)
+    subtitle = models.CharField(max_length=300 , default="There is no discription")
+    discription = models.TextField(max_length=10000)
     created_at = models.DateTimeField(default=datetime.now)
